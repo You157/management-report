6e1a9219a314ede77dce4ba8dc5825cd792d6b94
You157 <youshimo26@gmail.com>
2020-02-08 15:12:58 +0900

    2020.2.8.15.22

---
+++ docx_simple_service.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  8 14:09:35 2020
+
+@author: Okamoto-DRIHA001
+"""
+
+from docx import Document
+from docx.shared import RGBColor
+from docx.shared import Inches
+from docx.shared import Pt
+from docx.enum.text import WD_ALIGN_PARAGRAPH
+
+class SimpleDocxService:
+    
+    def __init__(self):
+        # コンストラクタ
+        self.document = Document()
+        self.latest_run = None
+
+    def set_normal_font(self, name, size):
+        # フォントの設定
+        font = self.document.styles['Normal'].font
+        font.name = name
+        font.size = Pt(size)
+
+    def add_head(self, text, lv):
+        # 見出しの設定
+        self.document.add_heading(text, level=lv)
+
+    def open_text(self):
+        # テキスト追加開始
+        self.paragraph = self.document.add_paragraph()
+
+    def close_text(self):
+        # テキスト追加終了
+        return # 現状では特に処理はなし
+
+    def get_unicode_text(self, text, src_code):
+        # python-docxで扱えるようにunicodeに変換
+        return unicode(text, src_code)
+
+    def adjust_return_code(self, text):
+        # テキストファイルのデータをそのままaddすると改行が面倒なことになるので、それを削除
+        text = text.replace("\n", "")
+        text = text.replace("\r", "")
+        return text
+
+    def add_text(self, text):
+        # テキスト追加
+        self.latest_run = self.paragraph.add_run(text)
+
+    def add_text_italic(self, text):
+        # テキスト追加（イタリックに）
+        self.paragraph.add_run(text).italic = True
+
+    def add_text_bold(self, text):
+        # テキスト追加（強調）
+        self.paragraph.add_run(text).bold = True
+
+    def add_text_color(self, text, r, g, b):
+        # 文字に色をつける
+        self.paragraph.add_run(text).font.color.rgb = RGBColor(r, g, b)
+
+    def paragraph_alignment_center(self):
+        # テキストを中央に配置
+        print("paragraph_alignment_center()")
+        # paragraph_format = self.paragraph.paragraph_format
+        # paragraph_format.alignment = WD_ALIGN_PARAGRAPH.CENTERR
+        
+
+    def add_picture(self, filename, inch):
+        # 図を挿入する
+        self.document.add_picture(filename, width=Inches(inch))
+
+    def add_table(self):
+        # 表を挿入する
+        pass
+
+    def add_page_break(self):
+        # 改ページを行う
+        self.document.add_page_break()
+
+    def save(self, name):
+        # docxファイルとして出力。
+        self.document.save(name)
